e91a32ff17f64e9602ab5eb60f7e73971c08ceb2
dawiddd95 <dawlyc@wp.pl>
2020-05-04 15:54:37 +0200

    UserDao dao created

--- src/main/java/model/UserRole.java
+++ src/main/java/model/UserRole.java
@@ -1,4 +1,34 @@
 package model;
 
+import model.enums.Role;
+
 public class UserRole {
+    private Integer id;
+    private Role role;
+
+    public UserRole(Integer id, Role role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public UserRole(Role role) {
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "id=" + id +
+                ", role=" + role +
+                '}';
+    }
+
 }
