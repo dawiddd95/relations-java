8c984bd742904f9b640c0c20e738a22bbb45ce5d
dawiddd95 <dawlyc@wp.pl>
2020-05-04 16:09:30 +0200

    UserRoleDao dao getAllUserRoles/getRoleById/getEolrIdByName methods added

--- src/main/java/dao/UserRoleDao.java
+++ src/main/java/dao/UserRoleDao.java
@@ -1,7 +1,11 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import model.UserRole;
+import model.enums.Role;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class UserRoleDao {
     private Connection connection;
@@ -24,5 +28,61 @@ public class UserRoleDao {
             e.printStackTrace();
         }
     }
+
+    public List<UserRole> getAllUserRoles() {
+        List<UserRole> userRoles = new LinkedList<UserRole>();
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String query = "select * from " + tableName;
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String name = resultSet.getString("role");
+                userRoles.add(new UserRole(id, Role.valueOf(name)));
+            }
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userRoles;
+    }
+
+    public UserRole getRoleById(Integer id) {
+        Statement statement = null;
+        try {
+            String query = "select * from " + tableName + " where id = '" + id + "'";
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                String role = resultSet.getString("role");
+                Role userRole = Role.valueOf(role);
+                return new UserRole(id, userRole);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Integer getRoleIdByName(String roleName) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String query = "select * from " + tableName + " where role = '" + roleName + "'";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                return id;
+            }
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
 
 }
