9b1952dad5d2133036783a9007a039253dd957e1
dawiddd95 <dawlyc@wp.pl>
2020-05-04 16:00:30 +0200

    UserDao dao updated

--- src/main/java/dao/UserRoleDao.java
+++ src/main/java/dao/UserRoleDao.java
@@ -1,4 +1,28 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class UserRoleDao {
+    private Connection connection;
+    private final String databaseName = "people";
+    private final String tableName = "roles";
+    private final String user = "root";
+    private final String password = "admin";
+
+    public UserRoleDao() {
+        init();
+    }
+
+    private void init() {
+        try {
+            // dla mysql => "com.mysql.jdbc.Driver"
+            Class.forName("org.postgresql.Driver");
+            // dla mysql => "jdbc:mysql://localhost/"+databaseName+"?useSSL=false"
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost/"+databaseName+"?useSSL=false", user, password);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
